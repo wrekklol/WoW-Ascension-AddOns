80690481ce4ffc87741c5910cd51f04e733561c1
wrekklol <kevin.sucks123@gmail.com>
2022-02-20 09:10:23 +0100

    .

---
+++ ZygorGuidesViewer/Ver.lua
@@ -0,0 +1,5 @@
+assert(ZygorGuidesViewer,"Zygor Guides Viewer failed to load.")
+ZygorGuidesViewer.revision = tonumber(string.sub("$Revision: 1196 $", 12, -3))
+ZygorGuidesViewer.version = "2.0." .. ZygorGuidesViewer.revision
+ZygorGuidesViewer.date = string.sub("$Date: 2010-06-28 15:55:27 -0500 (Mon, 28 Jun 2010) $", 8, 17)
+--2010/06/28 16:54:53.52442
