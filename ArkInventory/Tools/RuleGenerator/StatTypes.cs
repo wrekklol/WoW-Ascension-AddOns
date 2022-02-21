e3ccd77ff7f7ceec88656c027cfcec46fc6bcc87
wrekklol <kevin.sucks123@gmail.com>
2022-02-21 02:37:24 +0100

    .

--- ArkInventory/Tools/RuleGenerator/StatTypes.cs
+++ ArkInventory/Tools/RuleGenerator/StatTypes.cs
@@ -13,19 +13,19 @@ namespace RuleGenerator
             switch (InStatType)
             {
                 case EStatType.Stamina:
-                    return "tooltip(\"%+%d+ stamina\")";
+                    return "tooltip(\\\"%+%d+ stamina\\\")";
                 case EStatType.Strength:
-                    return "tooltip(\"%+%d+ strength\")";
+                    return "tooltip(\\\"%+%d+ strength\\\")";
                 case EStatType.Agility:
-                    return "tooltip(\"%+%d+ agility\")";
+                    return "tooltip(\\\"%+%d+ agility\\\")";
                 case EStatType.Intellect:
-                    return "tooltip(\"%+%d+ intellect\")";
+                    return "tooltip(\\\"%+%d+ intellect\\\")";
                 case EStatType.Spirit:
-                    return "tooltip(\"%+%d+ spirit\")";
+                    return "tooltip(\\\"%+%d+ spirit\\\")";
                 case EStatType.SpellPower:
-                    return "tooltip(\"%+%d+ spell power\", \"increases spell power by %d+\")";
+                    return "tooltip(\\\"%+%d+ spell power\\\", \\\"increases spell power by %d+\\\")";
                 case EStatType.AttackPower:
-                    return "tooltip(\"%+%d+ attack power\", \"increases attack power by %d+\")";
+                    return "tooltip(\\\"%+%d+ attack power\\\", \\\"increases attack power by %d+\\\")";
             }
 
             return "";
